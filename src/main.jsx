0d1e32d59e8c213e47b0b93922a41f9209dec920
Zikora <zikorapascal@gmail.com>
2025-12-03 20:18:46 +0000

    es6 testing

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
-import { createRoot } from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 
 createRoot(document.getElementById('root')).render(
-  <h1>Hello React</h1>
+  <h1>Hello my neighbors</h1>
 )
