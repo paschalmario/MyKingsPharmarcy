666fe1fa77f1e0fccddd4816e89f051474cd03d7
Zikora <zikorapascal@gmail.com>
2025-12-03 21:31:48 +0000

    additon

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,23 @@
 import {createRoot} from 'react-dom/client'
 
+const users = [
+  {id:1, name:"joana", age: 23},
+  {id:2, name:"Benjamin", age:32},
+  {id:3, name:"Franklin", age:21}
+]
+
+function Userlist(){
+  return(
+    <ul>
+      {users.map(user => 
+        <li key={user.id}>
+          {user.name} is {user.age} years old.
+        </li>
+      )}
+    </ul>
+  )
+}
+
 createRoot(document.getElementById('root')).render(
-  <h1>Hello my neighbors</h1>
+  <Userlist />
 )
