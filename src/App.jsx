1844b04459d33da63fb575962d110dda0edc6a31
Zikora <zikorapascal@gmail.com>
2025-12-04 01:40:02 +0000

    create cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import './App.css'
+function Card() {
+  const fruits = [
+    {id: 1, name:'Zikora', age:21},
+    {id: 2, name:'xino', age:22},
+    {id: 3, name:'John', age:23}
+  ]
+
+  return (
+    <>
+      <div>
+        {fruits.map(fruit => (
+          <div key={fruit.id} div className="card">
+            <h3>My name is {fruit.name}</h3><br />
+            <p>I am {fruit.age} years old</p>
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
 function App() {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <Card />
       </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,6 @@
 import {createRoot} from 'react-dom/client'
-
-const users = [
-  {id:1, name:"joana", age: 23},
-  {id:2, name:"Benjamin", age:32},
-  {id:3, name:"Franklin", age:21}
-]
-
-function Userlist(){
-  return(
-    <ul>
-      {users.map(user => 
-        <li key={user.id}>
-          {user.name} is {user.age} years old.
-        </li>
-      )}
-    </ul>
-  )
-}
-
+import App from './App'
 createRoot(document.getElementById('root')).render(
-  <Userlist />
+  <App/>
+  
 )
