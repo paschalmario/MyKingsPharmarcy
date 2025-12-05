65a1920cd94222e1f33472dd60e51ed6aea7db81
Zikora <zikorapascal@gmail.com>
2025-12-05 22:56:16 +0000

    test prop

---
+++ src/counter.jsx
@@ -0,0 +1,9 @@
+function PropTest(val){
+    return (
+        <>
+        <h1>Hello my name is {val.name}</h1>
+        <p>I am {val.age} years old</p>
+        </>
+    )
+}
+ export default PropTest;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import {createRoot} from 'react-dom/client'
 import App from './App'
+import PropTest from './counter'
+
+
+
 createRoot(document.getElementById('root')).render(
-  <App/>
+  <PropTest/>
   
 )
