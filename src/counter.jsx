ec883d5a86ef9289b2ec92ae728ce4c644e71407
Zikora <zikorapascal@gmail.com>
2025-12-06 18:40:33 +0000

    update react counter app

--- src/counter.jsx
+++ src/counter.jsx
@@ -1,9 +1,41 @@
-function PropTest(val){
-    return (
+import { useState } from "react";
+
+// function MyForm(){
+//     const [name, setName] = useState("ford")
+
+//     const handlechange = (event) => {
+//         setName(event.target.value)
+//     }
+
+//     return(
+//         <select value={name} onChange={handlechange}>
+//             <option value="ford">ford</option>
+//             <option value="BMW">BMW</option>
+//             <option value="nissan">nissan</option>
+//             <option value="tesla">tesla</option>
+//         </select>
+//     )
+   
+// }
+function Counter(){
+    var [num, Setnum] = useState(0);
+
+    function add() {
+       Setnum(num +1)
+    }
+    function minus(){
+        Setnum(num -1)
+    }
+    function handlechange(e){
+        Setnum(e.target.value)
+    }
+    return(
         <>
-        <h1>Hello my name is {val.name}</h1>
-        <p>I am {val.age} years old</p>
+        <button onClick={add}>+</button>
+        <p>{num}</p>
+        <button onClick={minus}>-</button>
         </>
+        
     )
 }
- export default PropTest;
+ export default Counter;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import {createRoot} from 'react-dom/client'
 import App from './App'
-import PropTest from './counter'
+import Counter from './counter'
 
 
 
 createRoot(document.getElementById('root')).render(
-  <PropTest/>
+  <Counter />
   
 )
